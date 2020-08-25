67be6a3c3fe7414945d9b4ae7895a4d662e75e70
CourtneyBritney <courtney02alexander@gmail.com>
2020-08-25 11:39:00 +0200

    Added Files

---
+++ Section6/Classes/src/com/courtney/Car.java
@@ -0,0 +1,10 @@
+package com.courtney;
+
+public class Car {
+
+     private int doors;
+     private int wheels;
+     public String model;
+     private String engine;
+     private String colour;
+}
---
+++ Section6/Classes/src/com/courtney/Main.java
@@ -0,0 +1,10 @@
+package com.courtney;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Car lamborghini = new Car();    //object
+        Car audi = new Car();
+        lamborghini.model = "Urus";
+    }
+}
