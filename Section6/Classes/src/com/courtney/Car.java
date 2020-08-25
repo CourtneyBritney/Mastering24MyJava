632f826aae481dc71cdedcc85413f3e99eee96de
CourtneyBritney <courtney02alexander@gmail.com>
2020-08-25 12:26:28 +0200

    Added Files

--- Section6/Classes/src/com/courtney/Car.java
+++ Section6/Classes/src/com/courtney/Car.java
@@ -4,7 +4,21 @@ public class Car {
 
      private int doors;
      private int wheels;
-     public String model;
+     private String model;
      private String engine;
      private String colour;
+
+     public void setModel(String model) {
+          String validModel = model.toLowerCase();
+          if(validModel.equals("urus") || validModel.equals("audi")) {
+               this.model = model;    //this keyword allows you to access fields of the current class.
+          } else {
+               this.model = "Unknown";
+          }
+
+     }
+
+     public String getModel() {
+         return this.model;
+     }
 }
