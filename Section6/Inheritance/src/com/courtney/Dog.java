3b6a88fe12d851efc0ff4c9b1f4decf09346ed31
CourtneyBritney <courtney02alexander@gmail.com>
2020-08-27 15:01:24 +0200

    Added Files

--- Section6/Inheritance/src/com/courtney/Dog.java
+++ Section6/Inheritance/src/com/courtney/Dog.java
@@ -8,6 +8,18 @@ public class Dog extends Animal {
     private int teeth;
     private String coat;
 
+
+    private void moveLegs(int speed) {
+        System.out.println("Dog.moveLegs() called");
+    }
+
+    @Override
+    public void move(int speed) {
+        System.out.println("Dog.move() called");
+        moveLegs(speed);
+        super.move(speed);
+   }
+
     public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
         super(name, 1, 1, size, weight);     //Calling the Super class, The big class which in this case its the Animal class
         this.eyes = eyes;
@@ -27,5 +39,15 @@ public class Dog extends Animal {
         System.out.println("Dog.eat() called");
         chew();
         super.eat();
+    }
+
+    public void walk() {
+        System.out.println("Dog.walk() called");
+        super.move(5);
+    }
+
+    public void run() {
+        System.out.println("Dog.run() called");
+        move(10);
     }
 }
