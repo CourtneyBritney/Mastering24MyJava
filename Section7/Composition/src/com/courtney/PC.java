990a5dd16184684849c07307828847325a383711
CourtneyBritney <courtney02alexander@gmail.com>
2020-08-31 17:02:34 +0200

    Section 7

---
+++ Section7/Composition/src/com/courtney/PC.java
@@ -0,0 +1,26 @@
+package com.courtney;
+
+public class PC {
+    //This extends from all three classes at once instead of extending from 1 class only
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public Case getTheCase() {
+        return theCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
+}
