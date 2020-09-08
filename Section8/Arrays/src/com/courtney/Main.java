32eefd8718a297aafd24b725cded586d330a6cb9
CourtneyBritney <courtney02alexander@gmail.com>
2020-09-08 12:42:59 +0200

    Added Files

--- Section8/Arrays/src/com/courtney/Main.java
+++ Section8/Arrays/src/com/courtney/Main.java
@@ -3,7 +3,6 @@ package com.courtney;
 import java.util.Scanner;
 
 public class Main {
-
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
