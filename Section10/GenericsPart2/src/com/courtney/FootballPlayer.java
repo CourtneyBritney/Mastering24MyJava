85c707c77142bd8f192579ff51f11586729f8300
CourtneyBritney <courtney02alexander@gmail.com>
2020-11-04 08:04:08 +0200

    Added Files

--- Section10/GenericsPart2/src/com/courtney/FootballPlayer.java
+++ Section10/GenericsPart2/src/com/courtney/FootballPlayer.java
@@ -1,6 +1,7 @@
 package com.courtney;
 
 public class FootballPlayer extends Player {
+
     public FootballPlayer(String name) {
         super(name);
     }
--- Section10/GenericsPart2/src/com/courtney/SoccerPlayer.java
+++ Section10/GenericsPart2/src/com/courtney/SoccerPlayer.java
@@ -1,6 +1,7 @@
 package com.courtney;
 
 public class SoccerPlayer extends Player {
+
     public SoccerPlayer(String name) {
         super(name);
     }
