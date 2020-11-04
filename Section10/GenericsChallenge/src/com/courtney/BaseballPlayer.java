e9591fa6fbeabd3405087f702d3d43accdf3d107
CourtneyBritney <courtney02alexander@gmail.com>
2020-11-04 08:36:27 +0200

    Added Files

---
+++ Section10/GenericsChallenge/src/com/courtney/BaseballPlayer.java
@@ -0,0 +1,7 @@
+package com.courtney;
+
+public class BaseballPlayer extends Player {
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ Section10/GenericsChallenge/src/com/courtney/FootballPlayer.java
@@ -0,0 +1,8 @@
+package com.courtney;
+
+public class FootballPlayer extends Player {
+
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
