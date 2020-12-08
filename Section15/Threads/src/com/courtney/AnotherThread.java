b2025766f4f49a1b735106afeda4115d4c38e446
CourtneyBritney <courtney02alexander@gmail.com>
2020-12-08 17:38:31 +0200

    Added Files

---
+++ Section15/Threads/src/com/courtney/AnotherThread.java
@@ -0,0 +1,11 @@
+package com.courtney;
+
+import static com.courtney.ThreadColor.ANSI_BLUE;
+
+public class AnotherThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_BLUE + "Hello from another thread.");
+    }
+}
